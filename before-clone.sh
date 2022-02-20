b2bd49bea0f0eeb875fe3a77b4e8003de65c1f5c
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-20 16:56:51 +0100

    Update before-clone.sh

--- before-clone.sh
+++ before-clone.sh
@@ -11,6 +11,11 @@ service auditd stop
 #Install required software
 yum update --skip-broken -y
 yum install -y nano
+yum install -y wget
+
+#Get (latest) Scripts 
+wget https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/main/first-startup.sh /etc/profile.d/
+wget https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/main/ssh-finish.sh /etc/profile.d/
 
 #Yum CleanUp
 yum clean all
@@ -50,5 +55,15 @@ rm -rf ~root/.ssh/
 rm -f ~root/anaconda-ks.cfg
 
 ###Final Step (this will shutdown VM)
+echo
+echo "Now I will restart the Template and you can make 4 clones of it to continue with your PowerFlex installation."
+echo
+echo "Turn them one by one, not at the same time, to overcome any IP issues or so."
+echo
+echo "First login inside the VM and then Turn ON another one. Wait for the 'first-startup.sh' to finish before moving to other steps."
+echo
+echo "Template will shutdown in 15 seconds from now..." 
+echo
+sleep 15
 history -c 
 sys-unconfig
