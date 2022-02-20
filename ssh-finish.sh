0882f3b4329700f1430b6707364d2162051c1ddd
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-20 21:06:01 +0100

    Update ssh-finish.sh

--- ssh-finish.sh
+++ ssh-finish.sh
@@ -12,31 +12,32 @@ VM4_Hostname_var="flex3"
 
 #SSH key echange with other nodes
 if [ "$Current_Hostname" = $VM1_Hostname_var ]; then
-	ssh-copy-id -i ~/.ssh/id_rsa.pub flex1
-	ssh-copy-id -i ~/.ssh/id_rsa.pub flex2
-	ssh-copy-id -i ~/.ssh/id_rsa.pub flex3
+    ssh-copy-id -i ~/.ssh/id_rsa.pub flex1
+    ssh-copy-id -i ~/.ssh/id_rsa.pub flex2
+    ssh-copy-id -i ~/.ssh/id_rsa.pub flex3
     echo "SSH keys were exchanged succesfully."
-	echo
-	echo "Please try to login without password to other hosts with 'ssh hostname' e.g. 'ssh flex1' (see sll options with 'cat /etc/hosts')."
+    echo
+    echo "Please try to login without password to other hosts with 'ssh hostname' e.g. 'ssh flex1' (see sll options with 'cat /etc/hosts')."
 else
-	if [ "$Current_Hostname" = $VM2_Hostname_var ]; then
-		ssh-copy-id -i ~/.ssh/id_rsa.pub flex-gw
+    if [ "$Current_Hostname" = $VM2_Hostname_var ]; then
+        ssh-copy-id -i ~/.ssh/id_rsa.pub flex-gw
 		ssh-copy-id -i ~/.ssh/id_rsa.pub flex2
 		ssh-copy-id -i ~/.ssh/id_rsa.pub flex3
 		echo "SSH keys were exchanged succesfully."
 		echo
 		echo "Please try to login without password to other hosts with 'ssh hostname' e.g. ssh 'flex-gw' (see sll options with 'cat /etc/hosts')."
-	else
-		if [ "$Current_Hostname" = $VM3_Hostname_var ]; then
-			ssh-copy-id -i ~/.ssh/id_rsa.pub flex1
+    else
+        if [ "$Current_Hostname" = $VM3_Hostname_var ]; then
+		    ssh-copy-id -i ~/.ssh/id_rsa.pub flex1
 			ssh-copy-id -i ~/.ssh/id_rsa.pub flex-gw
 			ssh-copy-id -i ~/.ssh/id_rsa.pub flex3
 			echo "SSH keys were exchanged succesfully."
 			echo
 			echo "Please try to login without password to other hosts with 'ssh hostname' e.g. ssh 'flex-gw' (see sll options with 'cat /etc/hosts')."
-		else
-		fi
-	fi
+        else
+		    echo
+        fi
+    fi
 fi
 echo
 #Cleaning up startup scripts in ~/.bash_profile
