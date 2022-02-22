15fbfc1e8ffe5fad12d34d1cd9b0e26c55d39162
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-22 12:21:34 +0100

    Update first-startup-more.sh

--- first-startup-more.sh
+++ first-startup-more.sh
@@ -30,56 +30,56 @@ echo
 echo "Your hostname is $Current_Hostname"
 echo
 if [ "$Current_Hostname" = $vm1_hostname_var ] || [ "$Current_Hostname" = $vm2_hostname_var ] || [ "$Current_Hostname" = $vm3_hostname_var ] || [ "$Current_Hostname" = $vm4_hostname_var ]; then
-    echo "Hostname is already set correctly"
+  echo "Hostname is already set correctly"
 else
-    echo "What VM Template clone number is this (1,2,3 or 4)?"
-    read VMno_var
-    if [ $VMno_var = 1 ]; then
-    		hostnamectl set-hostname $vm1_hostname_var
-		NewHostName1=$(hostname)
-		echo
-		echo "Your new hostname is $NewHostName1"
-		echo
-		sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
-		echo
-		echo "DHCP records from the VM were deleted, now we will set static definition."
-		echo
-		echo 'IPADDR='$flexgwip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		#echo 'DNS2=8.8.8.8' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		#echo 'DNS3=8.8.4.4' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-		systemctl restart network
-		echo $flexgwip_var' flex-gw' >> /etc/hosts
-		echo $flex1ip_var' flex1' >> /etc/hosts
-		echo $flex2ip_var' flex2' >> /etc/hosts
-		echo $flex3ip_var' flex3' >> /etc/hosts
-		clear
-		echo "Now we will create SSH keys and register them to ther hosts..."
-		echo
-		echo "Please type 'yes' when asked and use the default installation root password"
-		echo
-		echo "Please wait until the last VM get here, then continue..."
-		echo
-		echo read -n 1 -r -s -p "VM4 just get here? Press any key to continue..."
-		clear
-		rm -rf /.ssh/
-		ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
-		#Nodes SSH registration
-		ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex1ip_var"
-		ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex2ip_var"
-		ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex3ip_var"
-    else
-    	if [ $VMno_var = 2 ]; then
-			hostnamectl set-hostname $vm2_hostname_var
-			NewHostName2=$(hostname)
-			echo
-            		echo "Your new hostname is $NewHostName2"
-            		echo
+  echo "What VM Template clone number is this (1,2,3 or 4)?"
+  read VMno_var
+  if [ $VMno_var = 1 ]; then
+    hostnamectl set-hostname $vm1_hostname_var
+    NewHostName1=$(hostname)
+	echo
+	echo "Your new hostname is $NewHostName1"
+	echo
+	sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo
+	echo "DHCP records from the VM were deleted, now we will set static definition."
+	echo
+	echo 'IPADDR='$flexgwip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	#echo 'DNS2=8.8.8.8' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	#echo 'DNS3=8.8.4.4' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	systemctl restart network
+	echo $flexgwip_var' flex-gw' >> /etc/hosts
+	echo $flex1ip_var' flex1' >> /etc/hosts
+	echo $flex2ip_var' flex2' >> /etc/hosts
+	echo $flex3ip_var' flex3' >> /etc/hosts
+	clear
+	echo "Now we will create SSH keys and register them to ther hosts..."
+	echo
+	echo "Please type 'yes' when asked and use the default installation root password"
+	echo
+	echo "Please wait until the last VM get here, then continue..."
+	echo
+	echo read -n 1 -r -s -p "VM4 just get here? Press any key to continue..."
+	clear
+	rm -rf /.ssh/
+	ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
+	#Nodes SSH registration
+	ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex1ip_var"
+	ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex2ip_var"
+	ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex3ip_var"
+  else
+    if [ $VMno_var = 2 ]; then
+	hostnamectl set-hostname $vm2_hostname_var
+	NewHostName2=$(hostname)
+	echo
+	echo "Your new hostname is $NewHostName2"
+            echo
 			sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 			echo
 			echo "DHCP records from the VM were deleted, now we will set static definition."
@@ -87,11 +87,11 @@ else
 			echo 'IPADDR='$flex1ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
-            		echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
+            echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			#echo 'DNS2=8.8.8.8' >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			#echo 'DNS3=8.8.4.4' >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-            		echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+            echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			systemctl restart network
 			echo $flexgwip_var' flex-gw' >> /etc/hosts
@@ -115,11 +115,11 @@ else
 			ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex3ip_var"
 		else
 		    if [ $VMno_var = 3 ]; then
-		    		hostnamectl set-hostname $vm3_hostname_var
-			    	NewHostName3=$(hostname)
-                		echo
-			    	echo "Your new hostname is $NewHostName3"
-                		echo
+				hostnamectl set-hostname $vm3_hostname_var
+			    NewHostName3=$(hostname)
+                echo
+			    echo "Your new hostname is $NewHostName3"
+                echo
 				sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 				echo
 				echo "DHCP records from the VM were deleted, now we will set static definition."
@@ -154,12 +154,12 @@ else
 				ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex1ip_var"
 				ssh-copy-id -i ~/.ssh/id_rsa.pub "$flex3ip_var"
 			else
-                		if [ $VMno_var = 4 ]; then
-                    			hostnamectl set-hostname $vm4_hostname_var
-                    			NewHostName4=$(hostname)
-                    			echo
-                    			echo "Your new hostname is $NewHostName4"
-			        	echo
+                if [ $VMno_var = 4 ]; then
+                    hostnamectl set-hostname $vm4_hostname_var
+                    NewHostName4=$(hostname)
+                    echo
+                    echo "Your new hostname is $NewHostName4"
+			        echo
 					sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 					echo
 					echo "DHCP records from the VM were deleted, now we will set static definition."
