55564d1755e0aa35ce1883ede8c4fe01fd1b4e71
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-22 14:52:19 +0100

    Update ssh-finish.sh

--- ssh-finish.sh
+++ ssh-finish.sh
@@ -3,7 +3,6 @@
 #Show current hostname
 Current_Hostname=$(hostname)
 
-#Hostnames
 #Defined Hostnames:
 VM1_Hostname_var="flex-gw"
 VM2_Hostname_var="flex1"
@@ -35,7 +34,17 @@ else
 			echo
 			echo "Please try to login without password to other hosts with 'ssh hostname' e.g. ssh 'flex-gw' (see sll options with 'cat /etc/hosts')."
         else
-		    echo
+		    if [ "$Current_Hostname" = $VM4_Hostname_var ]; then
+				ssh-copy-id -i ~/.ssh/id_rsa.pub flex1
+				ssh-copy-id -i ~/.ssh/id_rsa.pub flex-gw
+				ssh-copy-id -i ~/.ssh/id_rsa.pub flex2
+				echo "SSH keys were exchanged succesfully."
+				echo
+				echo "Please try to login without password to other hosts with 'ssh hostname' e.g. ssh 'flex-gw' (see sll options with 'cat /etc/hosts')."
+				echo
+			else
+				echo
+			fi
         fi
     fi
 fi
@@ -46,4 +55,6 @@ sed -i '' -e '$ d' ~/.bash_profile
 echo "Environmental preparation is finished. You can proceed with additional steps in your PowerFlex installation."
 echo
 echo "Thank you!"
+echo
+read -n 1 -r -s -p "Press any key to finish."
 echo
