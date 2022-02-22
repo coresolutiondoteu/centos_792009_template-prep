1da3712c26ad0a236f4ab205f71891affec1fca0
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-22 16:47:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,28 @@
 
 clear
 
-##Get (latest) Scripts 
+#Stop logging services
+service rsyslog stop 
+service auditd stop
+echo
+echo "Updating system may take some time, please wait..."
+echo
+#Install required software
+yum update --skip-broken -y -q
+yum install -y nano wget ntp -q
+
+##Get (latest) Scripts
 wget -P /home/ https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/more/flex-pre-clone.sh
 wget -P /home/ https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/more/first-startup-more.sh
 wget -P /home/ https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/more/ssh-finish.sh
 chmod +x /home/*.sh
 
-echo "Scritps were downloaded. please run '/home/flex-pre-clone.sh'"
 echo
-echo "Praparation of your first template will begin."
+echo "System was updated and did install wget, nano and ntp."
+echo
+echo "Scripts were downloaded, please run '/home/flex-pre-clone.sh'."
+echo
+echo "Praparation of your first template will begin with this script."
 echo
 echo "I will guide you through the preparation."
 echo
