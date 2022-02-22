596ac7b7e478c88b6833243c5ab5e61266779204
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-22 19:18:01 +0100

    Update flex-pre-clone.sh

--- flex-pre-clone.sh
+++ flex-pre-clone.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#Ideas
-#
-# echo "What is your future DNS IP/ NET GW IP address?"
-# We could ask if it is the same like now and if yes, then use it?
-#
-# We could test the Ip ping and if there is correct answwer we could continue with the script?
-# So we can be sure, that previous steps were done correctly.
-#
 
 
 
@@ -17,35 +9,33 @@ echo
 echo "Please make a snapshot on the VM and name it Fresh Install"
 echo
 read -n 1 -r -s -p "Done? Press any key to continue..."
-echo
+echo "NOTE:"
 echo "---------------------------------------------------------------------------"
-echo "Here is your default IP configuration from the DHCP in VMware Networking"
-echo "By Default VMware Networking DHCP uses addresses x.x.x.128 - x.x.x.254"
-echo "The best then would be to choose something like 192.168.xxx.3 till .127"
+echo " Here is your default IP configuration from the DHCP in VMware Networking  "
+echo "  By Default VMware Networking DHCP uses addresses x.x.x.128 - x.x.x.254"
+echo " The best then would be to choose something like 192.168.xxx.3 till .127"
 echo "Where for .xxx. you will use what VMware networking choosen for your VMNet"
 echo "---------------------------------------------------------------------------"
 echo
 #Add static IP information, based on your IP requirments
 Current_IP=$(ip add | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/24')
 echo "Your current IP address is: $Current_IP"
-echo
 Current_Gateway=$(ip route show default | awk '/default/ {print $3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo "Your current default Gateway is: $Current_Gateway"
-echo
 Current_DNS=$(cat /etc/resolv.conf | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 echo "Your current DNS server is: $Current_DNS"
-echo
-echo "Now we can proceed to the next steps, I will need from you some imputs..."
 echo 
 echo "Please prepare your definied IP networking information:"
-echo
-echo "flex-gw IP >> Please choose new one e.g. 192.168.200.10"
-echo "flex1 IP >> Please choose new one e.g. 192.168.200.11"
-echo "flex2 IP >> Please choose new one e.g. 192.168.200.12"
-echo "flex3 IP >> Please choose new one e.g. 192.168.200.13"
-echo "Network PREFIX >> This maybe the same like it is now: 24"
-echo "GATEWAY IP >> This maybe the same like it is now: "$Current_Gateway""
-echo "DNS IP >> This maybe the same like it is now: "$Current_DNS""
+echo "---------------------------------------------------------------------------"
+echo "flex-gw IP      >>   Please choose new one (e.g. 192.168.200.10)"
+echo "flex1 IP        >>   Please choose new one (e.g. 192.168.200.11)"
+echo "flex2 IP        >>   Please choose new one (e.g. 192.168.200.12)"
+echo "flex3 IP        >>   Please choose new one (e.g. 192.168.200.13)"
+echo "Network PREFIX  >>   This maybe the same like it is now: 24"
+echo "GATEWAY IP      >>   This maybe the same like it is now: "$Current_Gateway""
+echo "DNS IP          >>   This maybe the same like it is now: "$Current_DNS""
+echo "---------------------------------------------------------------------------"
+
 
 #These are predefined variables for this installation
 echo 'flex-gw' >> /home/flex-gw.vars
@@ -56,58 +46,32 @@ echo
 echo "What is your future STATIC flex-gw IP address?"
 read flexgwip_var
 echo $flexgwip_var >> /home/flexgwip.vars
-echo 
 echo "What is your future STATIC flex1 IP address?"
 read flex1ip_var
 echo $flex1ip_var >> /home/flex1ip.vars
-echo
 echo "What is your future STATIC flex2 IP address?"
 read flex2ip_var
 echo $flex2ip_var >> /home/flex2ip.vars
-echo
 echo "What is your future STATIC flex3 IP address?"
 read flex3ip_var
 echo $flex3ip_var >> /home/flex3ip.vars
-echo
 echo "What is your future Network PREFIX? (e.g. for /16 write 16 and for /24 write 24)"
 read prefix_var
 echo $prefix_var >> /home/prefix.vars
-echo
 echo "What is your future Network GATEWAY IP address?"
-# We could ask if it is the same like now and if yes, then use it?
 read netgwip_var
 echo $netgwip_var >> /home/netgwip.vars
-echo
 echo "What is your future Network DNS IP address?"
-# We could ask if it is the same like now and if yes, then use it?
 read dnsip_var
 echo $dnsip_var >> /home/dnsip.vars
 echo
 echo "Thank you, we have set variables for flex nodes preparation."
 echo
-read -n 1 -r -s -p "All good? Press any key to continue..."
+sleep 2
+clear
 echo
-echo "Next process will be automatic and at the end VM will shutdown"
-echo
-echo "Then you can make 4 clones from your newly prepared template VM"
-echo
-read -n 1 -r -s -p "I am ready to generalize your template, press any key to continue..."
 
-
-##Pre-Clone script defionition
-#echo
-#Stop logging services
-#service rsyslog stop 
-#service auditd stop
-
-#Install required software
-#yum update --skip-broken -y
-#yum install -y nano wget ntp
-
-##Get (latest) Scripts 
-#wget -P /home/ https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/more/first-startup-more.sh
-#wget -P /home/ https://raw.githubusercontent.com/coresolutiondoteu/centos_792009_template-prep/more/ssh-finish.sh
-#chmod +x /home/*.sh
+#Next script after reboot
 echo ' ' >> ~/.bash_profile
 echo '#InstallationPowerFlex' >> ~/.bash_profile
 echo '/home/first-startup-more.sh' >> ~/.bash_profile
@@ -149,17 +113,22 @@ unset HISTFILE
 rm -rf ~root/.ssh/
 rm -f ~root/anaconda-ks.cfg
 
-###Final Step (this will shutdown VM)
+clear
 echo
-echo "/////////////////////////////////////////////////////////////////////"
-echo "Now I will Turn Off the Template VM, and you can make 4 clones"
-echo "from this Template to continue with your PowerFlex installation."
+echo "INFO: Image generalized."
+#Final Step (this will shutdown VM)
 echo
-echo "Start them one by one, not at the same time."
-echo
-echo "First login inside the turned on VM and only after start another one."
-echo "Wait for the startup script finish before moving to other steps."
-echo "/////////////////////////////////////////////////////////////////////"
+echo "/////////////////////////////////////////////////////////////////////////"
+echo "/                                                                       /"
+echo "/    Now I will Turn Off the Template VM, and you can make 4 clones     /"
+echo "/   from this Template to continue with your PowerFlex installation.    /"
+echo "/                                                                       /"
+echo "/             Start them one by one, not at the same time.              /" 
+echo "/                                                                       /"
+echo "/  Login inside the first started VM and only after start another one.  /"
+echo "/                        Please, follow guide...                        /"
+echo "/                                                                       /"
+echo "/////////////////////////////////////////////////////////////////////////"
 echo
 
 read -n 1 -r -s -p "Press any key to shutdown..."
