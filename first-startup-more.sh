d60c9fb5235033738d54e996e21b771d21fbb5d0
coresolutiondoteu <43107121+coresolutiondoteu@users.noreply.github.com>
2022-02-22 20:59:30 +0100

    Update first-startup-more.sh

--- first-startup-more.sh
+++ first-startup-more.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Reading all variables from /home/*.vars
-
 read vm1_hostname_var < /home/flex-gw.vars
 read vm2_hostname_var < /home/flex1.vars
 read vm3_hostname_var < /home/flex2.vars
@@ -12,7 +11,13 @@ read flex2ip_var < /home/flex2ip.vars
 read flex3ip_var < /home/flex3ip.vars
 read prefix_var < /home/prefix.vars
 read netgwip_var < /home/netgwip.vars
-read dnsip_var < /home/dnsip.vars 
+read dnsip_var < /home/dnsip.vars
+
+#Hosts file Update
+echo $flexgwip_var' flex-gw' >> /etc/hosts
+echo $flex1ip_var' flex1' >> /etc/hosts
+echo $flex2ip_var' flex2' >> /etc/hosts
+echo $flex3ip_var' flex3' >> /etc/hosts
 
 #NTPd
 systemctl start ntpd
@@ -47,12 +52,36 @@ else
         echo
         echo "Your new hostname is $NewHostName1"
         echo
-	read flexgwip_var < /home/flexgwip.vars
-		sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
+	sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo
+	echo "DHCP records from the VM were deleted, now we will set static definition."
+	echo
+	echo 'IPADDR='$flexgwip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	#echo 'DNS2=8.8.8.8' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	#echo 'DNS3=8.8.4.4' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
+	echo $flexgwip_var' flex-gw' >> /etc/hosts
+	echo $flex1ip_var' flex1' >> /etc/hosts
+	echo $flex2ip_var' flex2' >> /etc/hosts
+	echo $flex3ip_var' flex3' >> /etc/hosts
+	echo
+    else
+        if [ $VMno_var = 2 ]; then
+            	hostnamectl set-hostname $vm2_hostname_var
+            	NewHostName2=$(hostname)
+		echo
+            	echo "Your new hostname is $NewHostName2"
+            	echo
+	    	sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 		echo
 		echo "DHCP records from the VM were deleted, now we will set static definition."
 		echo
-		echo 'IPADDR='$flexgwip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+		echo 'IPADDR='$flex1ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 		echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 		echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
 		echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
@@ -66,18 +95,18 @@ else
 		echo $flex2ip_var' flex2' >> /etc/hosts
 		echo $flex3ip_var' flex3' >> /etc/hosts
 		echo
-    else
-        if [ $VMno_var = 2 ]; then
-            hostnamectl set-hostname $vm2_hostname_var
-            NewHostName2=$(hostname)
-			echo
-            echo "Your new hostname is $NewHostName2"
-            echo
+        else
+		if [ $VMno_var = 3 ]; then
+			hostnamectl set-hostname $vm3_hostname_var
+			NewHostName3=$(hostname)
+                	echo
+			echo "Your new hostname is $NewHostName3"
+                	echo
 			sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 			echo
 			echo "DHCP records from the VM were deleted, now we will set static definition."
 			echo
-			echo 'IPADDR='$flex1ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+			echo 'IPADDR='$flex2ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
 			echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
@@ -91,18 +120,18 @@ else
 			echo $flex2ip_var' flex2' >> /etc/hosts
 			echo $flex3ip_var' flex3' >> /etc/hosts
 			echo
-        else
-		    if [ $VMno_var = 3 ]; then
-			    hostnamectl set-hostname $vm3_hostname_var
-			    NewHostName3=$(hostname)
-                echo
-			    echo "Your new hostname is $NewHostName3"
-                echo
+		else
+                	if [ $VMno_var = 4 ]; then
+                    		hostnamectl set-hostname $vm4_hostname_var
+                    		NewHostName4=$(hostname)
+                    		echo
+                    		echo "Your new hostname is $NewHostName4"
+				echo
 				sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 				echo
 				echo "DHCP records from the VM were deleted, now we will set static definition."
 				echo
-				echo 'IPADDR='$flex2ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
+				echo 'IPADDR='$flex3ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 				echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
 				echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
 				echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
@@ -116,44 +145,17 @@ else
 				echo $flex2ip_var' flex2' >> /etc/hosts
 				echo $flex3ip_var' flex3' >> /etc/hosts
 				echo
-			else
-                if [ $VMno_var = 4 ]; then
-                    hostnamectl set-hostname $vm4_hostname_var
-                    NewHostName4=$(hostname)
-                    echo
-                    echo "Your new hostname is $NewHostName4"
-			        echo
-				read flexgwip_var < /home/flexgwip.vars
-read flex1ip_var < /home/flex1ip.vars
-read flex2ip_var < /home/flex2ip.vars
-read flex3ip_var < /home/flex3ip.vars
-					sed -i "s/BOOTPROTO=.*/BOOTPROTO="none"/" /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo
-					echo "DHCP records from the VM were deleted, now we will set static definition."
-					echo
-					echo 'IPADDR='$flex3ip_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'PREFIX='$prefix_var >>/etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'GATEWAY='$netgwip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'DNS1='$dnsip_var >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					#echo 'DNS2=8.8.8.8' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					#echo 'DNS3=8.8.4.4' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-ens33
-					echo $flexgwip_var' flex-gw' >> /etc/hosts
-					echo $flex1ip_var' flex1' >> /etc/hosts
-					echo $flex2ip_var' flex2' >> /etc/hosts
-					echo $flex3ip_var' flex3' >> /etc/hosts
-					echo
-                else
-                    echo
-				    echo "Only numbers in between 1 and up to 4 are allowed, but you did use $VMno_var which is illegal! Run this script again, please."
-                    echo
-                fi
-            fi
+                	else
+                		echo
+				echo "Only numbers in between 1 and up to 4 are allowed, but you did use $VMno_var which is illegal! Run this script again, please."
+                    		echo
+                	fi
+            	fi
         fi
     fi
 fi
+echo "Was hosts file updated?
+cat /etc/hosts
 
 
 sed -i '' -e '$ d' ~/.bash_profile
